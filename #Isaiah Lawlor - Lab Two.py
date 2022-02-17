8e5de151026ad8da0cd1eb0225c94014a2f8a9df
IsaiahL15 <Isaiah.Lawlor@gmail.com>
2022-02-16 20:41:28 -0500

    Create #Isaiah Lawlor - Lab Two.py

---
+++ #Isaiah Lawlor - Lab Two.py
@@ -0,0 +1,45 @@
+#Isaiah Lawlor - Lab Two
+# Strings
+# assigned outputs to 'speak' when input is inserted by user
+name = "Isaiah Lawlor"
+
+# Integers
+# This is a numeric value that is assigned as the particular output for a command inserted by the user
+age = 21
+
+# Floats 
+# another numeric value that has is represented with a decimal place. 
+weight = 168.7
+
+# Boolean 
+# This is a built in command that can inform the user of the truth or falsity of a line of code. 
+
+# Addition
+print(55+22)
+#Subtraction
+print(76-24)
+#Division
+print(99/11)
+#Multiplication
+print(19*14)
+#Modulo
+print(142%19)
+
+#Equation Demonstration
+print(84/12*8+1)
+
+#Change Excercise
+number_into_string = str(37)
+print(number_into_string)
+
+string_into_number = int(37)
+print(string_into_number)
+
+string_into_float = float(37)
+print(string_into_float)
+
+number_into_boolean = bool(37)
+print(number_into_boolean)
+
+#List Excercise
+New_England = ["Vermont","New Hampshire","Maine","Massachusetts","Rhode Island","Connecticut"]
